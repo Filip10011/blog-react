7298bfbc4c89e921c98d9274485729f239c33853
Filip Popovski <popovski_f@hotmail.com>
2023-01-09 20:35:05 +0100

    blogs and category components refactoring code

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { allBlogs } from "../../config/blogs";
 import "../Blogs/Blogs.css";
+import Category from "../Category/Category";
 
 const Blogs = () => {
   return (
@@ -8,11 +9,22 @@ const Blogs = () => {
       <div>
         {allBlogs.map((blog) => (
           <div key={blog.id}>
-            <h2>Category: {blog.category}</h2>
-            <h1>{blog.title}</h1>
-            <h3>Created at:{blog.createdAt}</h3>
-            <h3>Author name: {blog.authorName}</h3>
-            <img className="avatar" src={blog.authorAvatar} alt="" />
+            <img className="blog-item-cover" src={blog.cover} alt="cover" />
+            <Category label={blog.category} />
+            <h3>{blog.title}</h3>
+            <p className="blog-item-desc">{blog.description}</p>
+            <footer>
+              <div className="blog-item-author">
+                <img src={blog.authorAvatar} alt="avatar" />
+                <div>
+                  <h6>{blog.authorName}</h6>
+                  <p>{blog.createdAt}</p>
+                </div>
+              </div>
+              <a className="blog-item-link" href={`/blog/${blog.id}`}>
+                ➝
+              </a>
+            </footer>
           </div>
         ))}
       </div>
