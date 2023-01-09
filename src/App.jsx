156766e519b99be0ca236f6509638ff82ec956e4
Filip Popovski <popovski_f@hotmail.com>
2023-01-10 00:29:25 +0100

    final product

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Blog from "./pages/Blog/Blog";
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/blog/:id" element={<Blog />} />
         </Routes>
       </BrowserRouter>
     </div>
